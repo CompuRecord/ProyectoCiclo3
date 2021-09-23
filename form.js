bd9a7c5c9fb178775b654a2fa5b4207ef3cbc95e
Jonnier07 <jonniemendez07@gmail.com>
2021-09-23 16:36:35 -0500

    añadiendo apellido

--- form.js
+++ form.js
@@ -11,4 +11,23 @@ function checkNombre(nombre) {
         return false;
     }
 }
-module.exports = checkNombre;
+
+function checkApellido(Apellido) {
+    var regex = new RegExp('[a-z]{4,30}');
+    if (regex.test(apell.value)) {
+        alert("El apellido se ingreso correctamente")
+        return true;
+
+    } else if (apell === "") {
+        alert("El apellido no se ingreso correctamente")
+        return false;
+
+    } else {
+        alert("El apellido no se ingreso correctamente")
+        return fales;
+
+
+    };
+
+}
+module.exports = { checkNombre, checkApellido }
