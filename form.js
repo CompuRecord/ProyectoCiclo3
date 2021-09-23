1f63c47379a2d59a6e44c64beb15f6395bef7bc2
laura2ndrea <laura2ndrea12@gmail.com>
2021-09-23 16:16:44 -0500

    anadiendo formjs

---
+++ form.js
@@ -0,0 +1,15 @@
+function checkNombre() {
+    var nombre = document.getElementById("nombre").value;
+    var regex = new RegExp(/^[A-Za-z\_\-\.\s\xF1\xD1]+$/);
+    if (nombre === "") {
+        alert("El campo nombre está vacio");
+        return false;
+    } else if (nombre.length < 4 || nombre.length > 30) {
+        alert("El nombre debe tener entre 4 y 30 caracteres");
+        return false;
+    } else if (!regex.test(nombre)) {
+        alert("El nombre no acepta caracteres númericos");
+        return false;
+    }
+}
+module.exports = checkNombre;
