ceced9a64dd16f90d9bdffb9c16455fbd7495244
laura2ndrea <laura2ndrea12@gmail.com>
2021-09-30 18:25:16 -0500

    añadiendo primeras dos funciones

--- registro.js
+++ registro.js
@@ -1,7 +1,35 @@
-function agregarRegistro(){};
+registros=[];
+function agregarRegistro(){
+    let entradas = document.getElementsByClassName("input");
+    // for (var e = 0; e < 10; e++) {   
+         datos=[];
+         for (var i = 0; i < entradas.length; i++) { 
 
+             datos.push(entradas[i].value);
+             
+             //console.log (entradas[i].value);     
+         } 
+         registros.push(datos);   
+     //}  
+     
+    // limpiarFormulario(); 
+     console.log(registros)            
+};
 
-function ordenarArreglo(arreglo){};
+function ordenarArreglo(arreglo){
+    arreglo.sort((x, y) => {
+        const apellidox = x.apellido.toLowerCase();
+        const apellidoy = y.apellido.toLowerCase();
+        if (apellidox < apellidoy) {
+            return -1;
+        }
+        if (apellidox > apellidoy) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(arreglo);
+};
 
 
 function filtrarCorreo(arreglo){};
