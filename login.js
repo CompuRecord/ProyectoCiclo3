33c07aca798fd41d8a11a9f80a295977d2682003
laura2ndrea <laura2ndrea12@gmail.com>
2021-10-13 15:00:56 -0500

    función agregarRegistro

--- login.js
+++ login.js
@@ -5,7 +5,13 @@ function login() {
 }
 
 function agregarRegistro() {
-
+    arrayGuardar = {};
+    arrayGuardar.nombre = document.getElementById("nombre").value;
+    arrayGuardar.apellido = document.getElementById("apellido").value;
+    arrayGuardar.telefono = document.getElementById("telefono").value;
+    arrayGuardar.correo = document.getElementById("correo").value;
+    arrayGuardar.contraseña = document.getElementById("contrasena").value;
+    registros.push(arrayGuardar);
 }
 
 function validarCAPTCHA(valor) {
